6eb01c088f63f2fd8a87f658f4649a65582c2590
Aiden Kaminsky <aiden.kaminsky@live.biu.ac.il>
2021-05-10 20:06:05 +0300

    little fix, and add yarn

--- view/src/App.js
+++ view/src/App.js
@@ -6,25 +6,25 @@ function App() {
       <header className="App-header">
         <h1>Anomaly Detection Server</h1>
       </header>
-      <form action="/detect" method="POST" encType="multipart/form-data" className="searchFrom" target="result">
+      <form action="/detect" method="POST" encType="multipart/form-data" Name="searchFrom" target="result">
         <table className="tableFile" >
           <tr>
-            <td><input type="file" className="normal_file"/></td>
+            <td><input type="file" Name="normal_file"/></td>
             <td>קובץ טיסה רגילה</td>
           </tr>
           <tr>
-            <td><input type="file" className="test_file"/></td>
+            <td><input type="file" Name="test_file"/></td>
             <td>קובץ טיסה לבדיקה</td>
           </tr>
         </table>
 
           <div>
-            <input type="submit" value="UPLOAD" className="submit"/>
+            <input type="submit" value="UPLOAD" Name="submit"/>
           </div>
       </form>
 
       <div>
-        <iframe className="result" width="80%" height="500">Result will be here.</iframe>
+        <iframe Name="result" width="80%" height="500">Result will be here.</iframe>
       </div>
 
     </div>
